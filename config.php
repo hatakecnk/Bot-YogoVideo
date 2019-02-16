646090263a239809a3194953b363746c70eaacb2
xNot_Found <41625152+hatakecnk@users.noreply.github.com>
2019-02-16 16:55:36 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/* direkomendasikan set delay 40 detik */
+$set_delay=40;
+
+/* ubah xxxxx sesuai dengan punya kalian */ 
+$config=[
+"imei" => "xxxxx",
+"sign" => "xxxxx",
+"uuid" => "xxxxx",
+"token" => "xxxxx"
+];
